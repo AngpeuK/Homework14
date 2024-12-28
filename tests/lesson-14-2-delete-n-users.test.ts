551edccc53fb99031511580217408747ae44bb02
AngpeuK <angpeu@gmail.com>
2024-12-28 03:44:56 +0200

    Update lesson-14-2-delete-n-users.test.ts

--- tests/lesson-14-2-delete-n-users.test.ts
+++ tests/lesson-14-2-delete-n-users.test.ts
@@ -2,7 +2,7 @@ import {test, expect} from '@playwright/test'
 import {StatusCodes} from "http-status-codes"
 import {LessonApiClient} from "../src/controllers/lesson-api-client"
 
-let baseURL: string = 'https://node_app:3000'
+let baseURL: string = 'http://localhost:3000/users'
 
 
 test.describe('User management API', () => {
@@ -14,7 +14,7 @@ test.describe('User management API', () => {
         await apiClient.deleteUsers()
         const usersCount = await apiClient.createUsers(1)
         let userIDs = []
-        const response = await request.get(baseURL)
+        const response = await request.get(`${baseURL}`)
         const responseBody = await response.json()
         // get the number of objects in the array returned
         const numberOfObjects = responseBody.length
@@ -31,7 +31,7 @@ test.describe('User management API', () => {
             // validate the response status code
             expect.soft(response.status()).toBe(StatusCodes.OK)
         }
-        const expectResponse = await request.get(baseURL)
+        const expectResponse = await request.get(`${baseURL}`)
         const expectResponseBody = await expectResponse.json()
         expect(expectResponseBody).toStrictEqual([])
         await apiClient.deleteUsers()
