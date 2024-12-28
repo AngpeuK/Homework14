c13214b6326e246e6a6dcdd6e98441c8a7922eff
AngpeuK <angpeu@gmail.com>
2024-12-28 03:40:48 +0200

    Update homework-api-client.ts

--- src/controllers/homework-api-client.ts
+++ src/controllers/homework-api-client.ts
@@ -2,7 +2,7 @@ import {APIRequestContext} from 'playwright'
 import {expect} from "@playwright/test"
 import {StatusCodes} from "http-status-codes";
 
-let baseURL: string = 'https://node_app:3000'
+let baseURL: string = 'http://localhost:3000/users'
 
 export class HomeworkApiClient {
     static instance: HomeworkApiClient
@@ -22,33 +22,22 @@ export class HomeworkApiClient {
     }
 
     async createUsers(users: number): Promise<number> {
-    for (let i = 0; i < users; i++) {
-        let attempts = 0
-        const maxAttempts = 200
-
-        while (attempts < maxAttempts) {
-            const response = await this.request.post(baseURL)
-            if (!response.ok) {
-                console.error(`Ошибка: ${response.status} - ${response.statusText}`);
-                const errorBody = await response.text()  // Получить текстовый ответ для анализа
-                console.error(errorBody)
-                return
+        for (let i = 0; i < users; i++) {
+            let success = false
+            let attempts = 0
+            while (!success && attempts < 20) {
+                const response = await this.request.post(baseURL)
+                if (response.status() >= 200 && response.status() < 300) {
+                    success = true
+                }
+                attempts++
             }
-            const responseBody = await response.json()
-
-            
-            if (response.status() >= 200 && response.status() < 300) {
-                break
-            }
-            attempts++
         }
+        return users
     }
 
-    return users
-}
-
     async deleteUsers(count: number): Promise<void> {
-        const response = await this.request.get(baseURL)
+        const response = await this.request.get(`${baseURL}`)
         const responseBody = await response.json()
         const numberOfObjects = responseBody.length
         const usersToDelete = Math.min(count, numberOfObjects)
@@ -65,7 +54,7 @@ export class HomeworkApiClient {
     }
 
     async deleteAllUsers(): Promise<void> {
-        const response = await this.request.get(baseURL)
+        const response = await this.request.get(`${baseURL}`)
         const responseBody = await response.json()
         const numberOfObjects = responseBody.length
         let userIDs = [];
@@ -78,17 +67,9 @@ export class HomeworkApiClient {
             expect.soft(response.status()).toBe(StatusCodes.OK)
         }
     }
-
-    async checkAllUsersDeleted(): Promise<void> {
-        const response = await this.request.get(baseURL)
-        expect(response.status()).toBe(StatusCodes.OK)
-        const responseBody = await response.text()
-        expect(responseBody).toBe('[]');
-    }
-
-
+    
     async getUserDataByIndex(index: number): Promise<any> {
-        const response = await this.request.get(baseURL)
+        const response = await this.request.get(`${baseURL}`)
         const responseBody = await response.json()
 
         const users: any[] = []
